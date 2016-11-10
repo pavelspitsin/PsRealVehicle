976fd31f93260c5dedbee600d505172a6954fac4
Anton Rassadin <a.rassadin@corp.mail.ru>
2016-11-10 14:39:06 +0300

    AWM-5988 steering fixes 2

--- Source/PsRealVehiclePlugin/Private/PrvVehicleMovementComponent.cpp
+++ Source/PsRealVehiclePlugin/Private/PrvVehicleMovementComponent.cpp
@@ -868,7 +868,11 @@ void UPrvVehicleMovementComponent::UpdateBrake(float DeltaTime)
 	bSteeringStabilizerActiveLeft = false;
 	bSteeringStabilizerActiveRight = false;
 	
-	if (bSteeringStabilizer && (SteeringInput == 0.f) &&
+	const FVector LocalAngularVelocity = UpdatedMesh->GetComponentTransform().InverseTransformVectorNoScale(UpdatedMesh->GetPhysicsAngularVelocity());
+	const float LocalAngularSpeed = LocalAngularVelocity.Z;
+	const bool bEnableSteeringStabilizer = FMath::Sign(RawSteeringInput * LocalAngularSpeed) <= 0.f;
+	
+	if (bSteeringStabilizer && bEnableSteeringStabilizer &&
 		(HullAngularSpeed > SteeringStabilizerMinimumHullVelocity))		// Don't try to stabilize when we're to slow
 	{
 		// Smooth brake ratio up
