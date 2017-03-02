692be1168a43f56f2f9dbc8fca07acefaea75c61
v.alyamkin <ufna@ufna.ru>
2017-03-02 21:28:39 +0300

    Revert NaN check

--- Source/PsRealVehiclePlugin/Private/PrvVehicleMovementComponent.cpp
+++ Source/PsRealVehiclePlugin/Private/PrvVehicleMovementComponent.cpp
@@ -287,26 +287,16 @@ void UPrvVehicleMovementComponent::TickComponent(float DeltaTime, enum ELevelTic
 			{
 				// Time has come
 				// Set the body into it's meant position
-				if (CorrectionEndState.Position.ContainsNaN() == false &&
-					CorrectionEndState.Quaternion.ContainsNaN() == false &&
-					CorrectionEndState.LinVel.ContainsNaN() == false &&
-					CorrectionEndState.AngVel.ContainsNaN() == false)
-				{
-					UE_LOG(LogPrvVehicle, Warning, TEXT("Body correction force warp"));
+				UE_LOG(LogPrvVehicle, Warning, TEXT("Body correction force warp"));
 					
-					bCorrectionInProgress = false;
+				bCorrectionInProgress = false;
 					
-					FVector DeltaPos(FVector::ZeroVector);
-					FRigidBodyErrorCorrection ErrorCorrection;
-					ErrorCorrection.LinearDeltaThresholdSq = 0.f;
-					ErrorCorrection.AngularDeltaThreshold = 0.f;
+				FVector DeltaPos(FVector::ZeroVector);
+				FRigidBodyErrorCorrection ErrorCorrection;
+				ErrorCorrection.LinearDeltaThresholdSq = 0.f;
+				ErrorCorrection.AngularDeltaThreshold = 0.f;
 					
-					ApplyRigidBodyState(CorrectionEndState, ErrorCorrection, DeltaPos);
-				}
-				else
-				{
-					UE_LOG(LogPrvVehicle, Error, TEXT("Body correction force warp ignored because of NaN"));
-				}
+				ApplyRigidBodyState(CorrectionEndState, ErrorCorrection, DeltaPos);
 			}
 		}
 	}
